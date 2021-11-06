d1849676bacacb6ced1f882f5b236e4bc08a10d2
yokgs <yazid.slila01@gmail.com>
2021-11-06 19:10:04 +0100

    😥

--- src/core.js
+++ src/core.js
@@ -1,1 +1,50 @@
 let x = 0;
+let $ = {
+    iterate: function (table) {
+        if ($.hasNext(table)) {
+            let k = table.length - 1;
+            /*[
+                [8, 9, 5],
+                [-7, -9, 0]
+            ]*/
+            var min = Math.min(...table[k]);
+            var en = table[k].indexOf(min),
+                p = [];
+            for (let i = 1; i < table.length - 1; i++) {
+                let r = table[i].slice(0, table[0].length);
+                let t = r[en] / r[r.length - 1];
+                p = [...p, t];
+            }
+            var sr = p.indexOf(Math.min(...p)) + 1;
+            var table_ = JSON.parse(JSON.stringify(table));
+            for (let i = 1; i < table.length; i++) {
+                for (let j = 1; j < table[0].length; j++) {
+                    if (i == sr) {
+                        table[i][j] /= table_[i][en];
+                    } else if (j == en) {
+                        table[i][j]-=
+                    } else {
+
+                    }
+                }
+            }
+            return table;
+        }
+
+        return table;
+    },
+    hasNext: function (table) {
+        let k = table.length - 1;
+        return Math.min(...table[k]) < 0;
+    },
+    heatMap: function (old, table) {
+        return table;
+    },
+    valid: function (table) {
+        return true;
+    },
+    createTable: function (string) {
+
+    },
+
+}

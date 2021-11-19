b60e68b2722d009d3ee46dff58f36c82d221632d
yokgs <yazid.slila01@gmail.com>
2021-11-19 21:21:19 +0100

    +service worker

--- src/index.js
+++ src/index.js
@@ -99,3 +99,12 @@ let app = {
     }
 };
 Vue.createApp(app).mount('#app');
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+  }
